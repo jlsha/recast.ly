6aea9f23e295b33df51f713f831003a0e1444315
jlsha <jenniferlaurasha@gmail.com>
2017-01-07 17:45:01 -0800

    complete searchYoutube tests

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,14 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    part: 'snippet',
+    q: options.query,
+    maxResults: options.max,
+    key: options.key,
+    type: 'video',
+    videoEmbeddable: true   
+  })
+    .done(({items}) => { if (callback) { callback(items); } });
 };
 
 window.searchYouTube = searchYouTube;
